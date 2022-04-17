bda08ce04968b6fa0d8e476148a8d3595a071594
adrianespinoza1998 <adrianespinozaarevalo@gmail.com>
2022-04-17 13:52:24 -0400

    test validar so

--- src/helpers/validarSO.js
+++ src/helpers/validarSO.js
@@ -2,10 +2,15 @@ export const validarSO = () => {
 
     const navInfo = navigator.userAgentData.platform;
 
-    if (navInfo == 'Windows') {
+    /*if (navInfo == 'Windows') {
         return 'http://localhost:5000';
     }
     else {
         return 'http://3.16.54.133:80';
+    }*/
+    if(navInfo === null){
+        return 'http://3.16.54.133:80';
+    }else{
+        return 'http://localhost:5000';
     }
 }
