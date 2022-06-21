75a7142c5473085aeb660adeeabeb245cef1c999
adrianespinoza1998 <adrianespinozaarevalo@gmail.com>
2022-06-21 11:03:19 -0400

    add login and navbar

--- src/helpers/validarSO.js
+++ src/helpers/validarSO.js
@@ -1,6 +1,6 @@
 export const validarSO = () => {
 
-    const navInfo = navigator.userAgentData.platform;
+    //const navInfo = navigator.userAgentData.platform;
 
     /*if (navInfo == 'Windows') {
         return 'http://localhost:5000';
@@ -8,9 +8,7 @@ export const validarSO = () => {
     else {
         return 'http://3.16.54.133:80';
     }*/
-    if(navInfo === null){
-        return 'http://3.16.54.133:80';
-    }else{
-        return 'http://localhost:5000';
-    }
+
+    return 'http://3.16.54.133:80';
+
 }
